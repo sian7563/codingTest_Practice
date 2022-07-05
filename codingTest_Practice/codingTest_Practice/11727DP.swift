5d4acf37eec9eb964d6376a80f50995eaa3d7769
sian7563 <sian7563@naver.com>
2022-07-05 10:09:14 +0900

    11727DP

---
+++ codingTest_Practice/codingTest_Practice/11727DP.swift
@@ -0,0 +1,11 @@
+//
+//  11727DP.swift
+//  codingTest_Practice
+//
+//  Created by kimsian on 2022/07/05.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 1001)
