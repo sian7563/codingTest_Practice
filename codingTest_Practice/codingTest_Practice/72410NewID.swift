9ff3fb97ba436cb4ed8394e98033eb068ea8891f
sian7563 <sian7563@naver.com>
2022-07-05 14:39:25 +0900

    72410NewID

---
+++ codingTest_Practice/codingTest_Practice/72410NewID.swift
@@ -0,0 +1,39 @@
+//
+//  72410NewID.swift
+//  codingTest_Practice
+//
+//  Created by kimsian on 2022/07/05.
+//
+
+import Foundation
+
+func checkComma(_ char: String) -> String {
+    var result = char
+    if result.first == "." {
+        result.removeFirst(
+    } else if result.last == "." {
+        result.removeLast()
+    }
+    return result
+}
+
+func solution(_ new_id: String) -> String {
+    var result = new_id
+    result = result.lowercased()
+    result = result.replacingOccurrences(of: "[^0-9a-z._-]", with: "", options: .regularExpression)
+    result = result.replacingOccurrences(of: "[.]+", with: ".", options: .regularExpression)
+    result = checkComma(result)
+    result += result == "" ? "a" : ""
+    while result.count > 15 {
+        result.removeLast()
+    }
+    result = checkComma(result)
+    while result.count < 3 {
+        result += String(result.last!)
+    }
+    
+    return result
+}
+print(solution("...!@BaT#*..y.abcdefghijklm"))
+
+
